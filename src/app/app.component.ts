45b389d8b339f0c77c195c91780e87c2a4599276
May <mayyadanar@hotmail.com>
2023-02-23 16:19:55 +0700

    fixed production build audio error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent implements OnInit {
 
     title : string = "THOTH";
     isDarkMode : boolean = false;
-    isVolumeEnable : boolean = false;
+    isVolumeEnable : boolean = true;
     volumeIcon : string = "bi bi-volume-up-fill";
     modeIcon : string = "bi bi-moon-fill";
     body = document.querySelector('body');
